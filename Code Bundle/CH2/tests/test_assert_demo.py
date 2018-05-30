e26286440e66a3d382239d2a041cbd5b957f9e7e
Bruno Oliveira <nicoddemus@gmail.com>
2018-05-29 22:57:25 -0300

    Add more code from chapter 2

--- Code Bundle/CH2/tests/test_assert_demo.py
+++ Code Bundle/CH2/tests/test_assert_demo.py
@@ -1,18 +1,64 @@
 import textwrap
 from math import sqrt
 
+from pytest import approx
+
 
 def magnitude(x, y):
     return sqrt(x * x + y * y)
 
 
-def starting_coords():
-    return 2.2, 3.6
+def test_simple_math():
+    assert abs(0.1 + 0.2) - 0.3 < 0.0001
+
+
+def test_simple_math2():
+    assert (-0.1 - 0.2) + 0.3 < 0.0001
+
+
+def test_approx_simple():
+    assert 0.1 + 0.2 == approx(0.3)
+
+
+def test_approx_list():
+    assert [0.1 + 1.2, 0.2 + 0.8] == approx([1.3, 1.0])
+
+
+def test_approx_dict():
+    values = {'v1': 0.1 + 1.2, 'v2': 0.2 + 0.8}
+    assert values == approx(dict(v1=1.3, v2=1.0))
+
+
+def test_approx_simple_fail():
+    assert 0.1 + 0.2 == approx(0.35)
+
+
+def test_approx_list_fail():
+    assert [0.1 + 1.2, 0.2 + 0.8] == approx([1.3, 1.1])
+
+
+def test_approx_numpy():
+    import numpy as np
+    values = np.array([0.1, 0.2]) + np.array([1.2, 0.8])
+    assert values == approx(np.array([1.3, 1.0]))
+
+
+def test_magnitude_plain():
+    assert abs(magnitude(8.0, 20.0) - 21.540659) < 0.00001
 
 
 def test_magnitude():
-    x, y = starting_coords()
-    assert magnitude(x, y) == 8.5
+    assert magnitude(8.0, 20.0) == approx(21.540659)
+
+
+def get_default_health(class_name):
+    assert class_name == 'warrior'
+    return 80
+
+
+def test_default_health():
+    health = get_default_health('warrior')
+    assert health == 95
 
 
 def get_default_player_class():
@@ -50,3 +96,36 @@ def test_warrior_long_description():
         allow to yield heavy armor and weapons, as well as carry
         more equipment. Weak in magic.            
         ''')
+
+
+def get_starting_equipment(class_name):
+    assert class_name == 'warrior'
+    return ['long sword', 'warrior set', 'shield']
+
+
+def test_get_starting_equiment():
+    expected = ['long sword', 'shield']
+    assert get_starting_equipment('warrior') == expected
+
+
+def test_long_list():
+    x = [str(i) for i in range(100)]
+    y = [str(i) for i in range(0, 100, 2)]
+    assert x == y
+
+
+def get_classes_starting_health():
+    return {'warrior': 85, 'sorcerer': 55, 'knight': 95}
+
+
+def test_starting_health():
+    expected = {'warrior': 85, 'sorcerer': 50}
+    assert get_classes_starting_health() == expected
+
+
+def get_player_classes():
+    return {'warrior', 'knight', 'sorcerer'}
+
+
+def test_player_classes():
+    assert get_player_classes() == {'warrior', 'sorcerer'}
---
+++ Code Bundle/CH2/tests/test_checks.py
@@ -0,0 +1,63 @@
+import pytest
+
+
+class InvalidCharacterNameError(Exception):
+    pass
+
+
+class InvalidClassNameError(Exception):
+    pass
+
+
+class Character:
+    pass
+
+
+VALID_CLASSES = ['sorcerer', 'warrior']
+
+
+def create_character(name: str, class_name: str) -> Character:
+    """
+    Creates a new character and inserts it into the database.
+
+    :param name: the character name.
+
+    :param class_name: the character class name.
+
+    :raise InvalidCharacterNameError:
+        if the character name is empty.
+
+    :raise InvalidClassNameError:
+        if the class name is invalid.
+
+    :return: the newly created Character.
+    """
+    if not name:
+        raise InvalidCharacterNameError('character name empty')
+
+    if class_name not in VALID_CLASSES:
+        msg = f'invalid class name: "{class_name}"'
+        raise InvalidCharacterNameError(msg)
+    ...
+
+
+def test_empty_name():
+    with pytest.raises(InvalidCharacterNameError):
+        create_character(name='', class_name='warrior')
+
+
+def test_invalid_class_name():
+    with pytest.raises(InvalidClassNameError):
+        create_character(name='Solaire', class_name='mage')
+
+
+def test_empty_name():
+    with pytest.raises(InvalidCharacterNameError,
+                       match='character name empty'):
+        create_character(name='', class_name='warrior')
+
+
+def test_invalid_class_name():
+    with pytest.raises(InvalidClassNameError,
+                       match='invalid class name: "mage"'):
+        create_character(name='Solaire', class_name='mage')
