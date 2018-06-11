9a1be53342028f9fa9a6f9cfb79e507c078f6cfa
Bruno Oliveira <nicoddemus@gmail.com>
2018-06-11 20:23:05 -0300

    Add more skipif examples

--- Code Bundle/CH3/tests/test_skipif.py
+++ Code Bundle/CH3/tests/test_skipif.py
@@ -17,3 +17,32 @@ def test_spawn_server_using_fork():
 )
 def test_spawn_server_using_fork2():
     ...
+
+
+def initialize_graphics():
+    pass
+
+
+def supports_shaders():
+    return False
+
+
+def test_shaders():
+    initialize_graphics()
+    if not supports_shaders():
+        pytest.skip("shades not supported")
+    # rest of the test code
+    ...
+
+
+def test_tracers_as_arrays_manual():
+    try:
+        import numpy
+    except ImportError:
+        pytest.skip("requires numpy")
+    ...
+
+
+def test_tracers_as_arrays_114():
+    numpy = pytest.importorskip("numpy", minversion="1.14")
+    ...
