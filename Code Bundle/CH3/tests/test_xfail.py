001d21bcb042ce6c1ce8a3818265800a37f93ca3
Bruno Oliveira <nicoddemus@gmail.com>
2018-06-12 22:14:45 -0300

    Add xfail examples

---
+++ Code Bundle/CH3/tests/test_xfail.py
@@ -0,0 +1,43 @@
+import sys
+
+import pytest
+
+
+@pytest.mark.xfail
+def test_simulation_34():
+    ...
+
+
+@pytest.mark.xfail(
+    sys.platform.startswith("win"),
+    reason="flaky on Windows #42",
+    strict=False,
+)
+def test_login_dialog():
+    ...
+
+
+def check_credentials(name):
+    raise NotImplementedError
+
+
+@pytest.mark.xfail(
+    raises=NotImplementedError, reason="will be implemented in #987"
+)
+def test_credential_check():
+    check_credentials("User")  # not implemented yet
+
+
+class Particle:
+    pass
+
+
+def collide(p1, p2):
+    pass
+
+
+@pytest.mark.xfail(
+    run=False, reason="undefined particles cause a crash #625"
+)
+def test_undefined_particle_collision_crash():
+    collide(Particle(), Particle())
