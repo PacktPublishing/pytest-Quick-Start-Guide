d88e5fec0fda6dfd2f5d5182e3a42754a05b47e9
Bruno Oliveira <nicoddemus@gmail.com>
2018-06-20 21:55:39 -0300

    Add more examples for chapter 3

---
+++ Code Bundle/CH3/tests/test_formula.py
@@ -0,0 +1,92 @@
+import pytest
+
+
+class FormulaTokenizer(object):
+    pass
+
+
+class Formula(object):
+
+    @classmethod
+    def from_string(cls, param, tokenizer):
+        return cls()
+
+    def eval(self, **kwargs):
+        return 0.0
+
+
+def test_formula_parsing():
+    tokenizer = FormulaTokenizer()
+    formula = Formula.from_string("C0 * x + 10", tokenizer)
+    assert formula.eval(x=1.0, C0=2.0) == pytest.approx(12.0)
+
+    formula = Formula.from_string("sin(x) + 2 * cos(x)", tokenizer)
+    assert formula.eval(x=0.7) == pytest.approx(2.1739021)
+
+    formula = Formula.from_string("log(x) + 3", tokenizer)
+    assert formula.eval(x=2.71828182846) == pytest.approx(4.0)
+
+
+def test_formula_parsing2():
+    values = [
+        ("C0 * x + 10", dict(x=1.0, C0=2.0), 12.0),
+        ("sin(x) + 2 * cos(x)", dict(x=0.7), 2.1739021),
+        ("log(x) + 3", dict(x=2.71828182846), 4.0),
+    ]
+    tokenizer = FormulaTokenizer()
+    for formula, inputs, result in values:
+        formula = Formula.from_string(formula, tokenizer)
+        assert formula.eval(**inputs) == pytest.approx(result)
+
+
+@pytest.mark.parametrize(
+    "formula, inputs, result",
+    [
+        ("C0 * x + 10", dict(x=1.0, C0=2.0), 12.0),
+        ("sin(x) + 2 * cos(x)", dict(x=0.7), 2.1739021),
+        ("log(x) + 3", dict(x=2.71828182846), 4.0),
+        pytest.param(
+            "hypot(x, y)",
+            dict(x=3, y=4),
+            5.0,
+            marks=pytest.mark.xfail(reason="not implemented: #102"),
+        ),
+    ],
+)
+def test_formula(formula, inputs, result):
+    tokenizer = FormulaTokenizer()
+    formula = Formula.from_string(formula, tokenizer)
+    assert formula.eval(**inputs) == pytest.approx(result)
+
+
+@pytest.mark.parametrize(
+    "formula, inputs, result",
+    [
+        pytest.param("x + 3", dict(x=1.0), 4.0, id="add"),
+        pytest.param("x - 1", dict(x=6.0), 5.0, id="sub"),
+    ],
+)
+def test_formula_simple(formula, inputs, result):
+    tokenizer = FormulaTokenizer()
+    formula = Formula.from_string(formula, tokenizer)
+    assert formula.eval(**inputs) == pytest.approx(result)
+
+
+class TestSeparated:
+
+    def test_formula_linear(self):
+        tokenizer = FormulaTokenizer()
+        formula = Formula.from_string("C0 * x + 10", tokenizer)
+        assert formula.eval(x=1.0, C0=2.0) == pytest.approx(12.0)
+
+    def test_formula_sin_cos(self):
+        tokenizer = FormulaTokenizer()
+        formula = Formula.from_string(
+            "sin(x) + 2 * cos(x)", tokenizer
+        )
+        assert formula.eval(x=0.7) == pytest.approx(2.1739021)
+
+    def test_formula_log(self):
+        tokenizer = FormulaTokenizer()
+        formula = Formula.from_string("log(x) + 3", tokenizer)
+        assert formula.eval(x=2.71828182846) == pytest.approx(4.0)
--- Code Bundle/CH3/tests/test_parametrization.py
+++ Code Bundle/CH3/tests/test_parametrization.py
@@ -3,6 +3,7 @@ from enum import Enum
 from typing import Any
 
 import attr
+import pytest
 
 
 class ElementType(Enum):
@@ -136,8 +137,6 @@ class JSONSerializer:
     def _deserialize_quantity(self, data: str) -> Quantity:
         ...
 
-    # omit the other methods as they are similar to
-
     @serialize.register(Pipe)
     def _serialize_pipe(self, pipe: Pipe) -> str:
         ...
@@ -147,7 +146,7 @@ class JSONSerializer:
         ...
 
 
-class TestSerialization:
+class Test:
 
     def test_quantity(self):
         serializer = JSONSerializer()
@@ -159,8 +158,63 @@ class TestSerialization:
     def test_pipe(self):
         serializer = JSONSerializer()
         pipe = Pipe(
-            lenght=Quantity(1000, "m"), diameter=Quantity(35, "cm")
+            length=Quantity(1000, "m"), diameter=Quantity(35, "cm")
         )
-        data = serializer.serialize_pipe(pipe)
+        data = serializer.serialize(pipe)
+        new_pipe = serializer.deserialize(data)
+        assert new_pipe == pipe
+
+
+class XMLSerializer(JSONSerializer):
+    pass
+
+
+class YAMLSerializer(JSONSerializer):
+    pass
+
+
+class Test:
+
+    def test_quantity(self):
+        for serializer in [
+            JSONSerializer(), XMLSerializer(), YAMLSerializer()
+        ]:
+            quantity = Quantity(10, "m")
+            data = serializer.serialize(quantity)
+            new_quantity = serializer.deserialize(data)
+            assert new_quantity == quantity
+
+    def test_pipe(self):
+        for serializer in [
+            JSONSerializer(), XMLSerializer(), YAMLSerializer()
+        ]:
+            pipe = Pipe(
+                length=Quantity(1000, "m"),
+                diameter=Quantity(35, "cm"),
+            )
+            data = serializer.serialize(pipe)
+            new_pipe = serializer.deserialize(data)
+            assert new_pipe == pipe
+
+
+@pytest.mark.parametrize(
+    "serializer_class",
+    [JSONSerializer, XMLSerializer, YAMLSerializer],
+)
+class Test:
+
+    def test_quantity(self, serializer_class):
+        serializer = serializer_class()
+        quantity = Quantity(10, "m")
+        data = serializer.serialize(quantity)
+        new_quantity = serializer.deserialize(data)
+        assert new_quantity == quantity
+
+    def test_pipe(self, serializer_class):
+        serializer = serializer_class()
+        pipe = Pipe(
+            length=Quantity(1000, "m"), diameter=Quantity(35, "cm")
+        )
+        data = serializer.serialize(pipe)
         new_pipe = serializer.deserialize(data)
         assert new_pipe == pipe
