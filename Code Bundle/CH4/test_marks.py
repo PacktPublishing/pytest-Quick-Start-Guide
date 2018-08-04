bdb4b831e5b8125722ed45c7a6d1eea751662682
Bruno Oliveira <nicoddemus@gmail.com>
2018-08-04 13:09:48 -0300

    Update examples after review

--- Code Bundle/CH4/test_marks.py
+++ Code Bundle/CH4/test_marks.py
@@ -5,24 +5,24 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def setup_locale():
-    locale.setlocale(locale.LC_ALL, "us_EN")
+    locale.setlocale(locale.LC_ALL, "en_US")
     yield
     locale.setlocale(locale.LC_ALL, None)
 
 
-def test_numbers_us():
-    assert locale.format("%f", 10.5) == "10.500000"
+def test_currency_us():
+    assert locale.currency(10.5) == "$10.50"
 
 
 @pytest.fixture(autouse=True)
 def setup_locale(request):
-    mark = request.node.get_closest_marker("change_local")
-    loc = mark.args[0] if mark is not None else "us_EN"
+    mark = request.node.get_closest_marker("change_locale")
+    loc = mark.args[0] if mark is not None else "en_US"
     locale.setlocale(locale.LC_ALL, loc)
     yield
     locale.setlocale(locale.LC_ALL, None)
 
 
-@pytest.mark.change_local("pt_BR")
-def test_numbers_br():
-    assert locale.format("%f", 10.5) == "10,500000"
+@pytest.mark.change_locale("pt_BR")
+def test_currency_br():
+    assert locale.currency(10.5) == "R$ 10,50"
